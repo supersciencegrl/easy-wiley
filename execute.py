9b32f40b41a1217305ad6217779fe93a6d723990
supersciencegrl <methionine57@gmail.com>
2020-03-10 11:26:44 +0000

    2020-Mar-10

--- execute.py
+++ execute.py
@@ -2,9 +2,9 @@ import os
 
 from updater import getxml
 
-try:
-    os.chdir('C:\\Users\\Nessa\\Documents\\GitHub\\easy-wiley')
-except FileNotFoundError:
-    os.chdir('C:\\Users\\CARSOL02\\Documents\\GitHub\\easy-wiley\\easy-wiley')
+#try:
+#    os.chdir('C:\\Users\\Nessa\\Documents\\GitHub\\easy-wiley')
+#except FileNotFoundError:
+#    os.chdir('C:\\Users\\CARSOL02\\Documents\\GitHub\\easy-wiley\\easy-wiley')
 
 getxml()
