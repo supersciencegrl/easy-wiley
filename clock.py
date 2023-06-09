e714700da016d749a4d0852c351a47232a398060
Nessa Carson <methionine57@gmail.com>
2023-06-09 19:33:36 +0100

    Clean up old files

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+## DEPRECATED ##
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from getxml import updaterss
@@ -9,3 +11,18 @@ def scheduled_job():
     updaterss()
 
 sched.start()
+
+""" from apscheduler.schedulers.background import BackgroundScheduler
+
+from updater import getxml
+
+jobid = 'Wiley-update'
+
+def canceljob(jobid):
+    sched.remove_job(jobid)
+
+sched = BackgroundScheduler()
+sched.start()
+
+job = sched.add_job(getxml, 'interval', minutes = 86400, id = jobid)
+ """
